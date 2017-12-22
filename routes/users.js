ea1726b539c6de3fc281242f7b0d13e71c32ed5a
eutampieri <eugenio@etcloud.ddns.net>
2017-12-22 21:52:46 +0100

    Trying to use MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'cado';
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    res.send("Connected successfully to server");
+  
+    const db = client.db(dbName);
+  
+    client.close();
+  });
+  
 });
 
 module.exports = router;
