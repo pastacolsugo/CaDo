eaef2942ed5a686c3d90edbeb38c2d274dd42a4c
eutampieri <eugenio@serverseutampieri.ddns.net>
2017-12-23 14:19:49 +0100

    Now the utility.js has a template to implement a route serving files

--- routes/utility.js
+++ routes/utility.js
@@ -4,11 +4,12 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log ('utility');
+    console.log('utility');
+    res.render('index', { title: 'Utilities' });
 });
 
-router.get('/jquery', function(req, res, next){
+/*router.get('/jquery', function(req, res, next){
     res.sendFile(path.join(__dirname, '/../', 'public/jQuery.js'));
-});
+});*///Modifying theese commented lines you can serve files like frameworks
 
 module.exports = router;
