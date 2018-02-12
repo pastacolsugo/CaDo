9fd9b873bbe8f7cee51b98c1ac56a34abe060934
eutampieri <eugenio@serverseutampieri.ddns.net>
2018-02-12 18:42:50 +0100

    Added a missing file that prevented the code to run

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var fs = require('fs');
 
+function pwdgen() {
+    var dicFilePath = path.join(__dirname, '/../', 'private/en.dic');
+    var dic = fs.readFileSync(dicFilePath, "utf8").split("\n");
+    var pwdLen = 3;//Password length in words;
+    var pwdSeparator = "-";//Separator between words
+    var pwd = "";
+    for (var i = 0; i < pwdLen; i++){
+        var r = Math.random();
+        r *= dic.length;
+        r = (r - r % 1);
+        console.log(r);
+        if (i % 2 == 0) pwd += dic[r];
+        else pwd += dic[r].toUpperCase();
+        if (i < pwdLen - 1) pwd += '-';
+    }
+    return pwd;    
+}
 
 router.get('/contest', function (req, res, next) {
     //this function will serve user personalized contest details used to populate the main page
     res.type('.json');
     res.send(JSON.stringify({
-        "name": "Contest di prova",
+        "name": "Contest di prova "+pwdgen(),
         "end_date": new Date(new Date().getTime() + 3 * 3600000),
         "tasks": [
             {
@@ -57,9 +75,9 @@ router.get('/alerts', function (req, res, next) {
 });
 
 router.get('/task', function (req, res, next) {
-    res.setHeader('Content-disposition', 'attachment; filename='+req.query.task+'.pdf');//req.query.task is the name of the requested file
+    res.setHeader('Content-disposition', 'attachment; filename=' + req.query.task + '.pdf');//req.query.task is the name of the requested file
     res.type(".pdf");
     res.sendFile(path.join(__dirname, '/../', '/DatabaseScheme.pdf'))
-})
+});
 
 module.exports = router;
