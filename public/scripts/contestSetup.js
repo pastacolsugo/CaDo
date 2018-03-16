8b9691bf41883cd442c288000035e0cdaff6860a
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-03-16 19:11:47 +0100

    in contestSetup.js tried to add functionality that accordingly with number of teams provides inputs bar

--- public/scripts/contestSetup.js
+++ public/scripts/contestSetup.js
@@ -1,6 +1,6 @@
 // function to fetch the form content and organizing it into a pretty object
 
-var getFormData = function () {
+var postFormData = function () {
     // empty object initialization
     var contest = {};
     
@@ -18,4 +18,40 @@ var getFormData = function () {
     // console.log(contest);
     console.log(stringContest);
     // here instead of log, a help is needed to POST the stringContest var into a JSON file in the backend side
+}
+
+var setTeams = function() {
+    
+    var teamForm = document.getElementById('teamNumber');
+    var nTeams = 0;
+    try{
+        nTeams = parseInt(teamForm.value);
+    }
+    catch(err){
+        nTeams = 0;
+    }
+    
+    if (nTeams == 0 ){
+        var advise = document.createElement('h6');
+        advise.innerHTML = "Inserisci un numero di squadre diverso da 0";
+        teamForm.appendChild(advise);
+    }
+    else{
+        for(var i=0; i<nTeams; i++){
+            var newTeam = document.createElement("input");
+            newTeam.type = "text";
+            newTeam.id = "team " + i;
+            newTeam.class = "textInput";
+            newTeam.name = "nomeGara";
+            newTeam.placeholder = newTeam.id + "name";
+            newTeam.autocomplete = "false";
+            newTeam.autocorrect = "false";
+            newTeam.autocapitaliza = "false";
+            newTeam.spellcheck = "false";
+            
+            document.getElementById('form').appendChild(newTeam);
+
+        }
+    }
+
 }
